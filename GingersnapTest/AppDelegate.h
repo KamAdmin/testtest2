79b11b142e08be6d3ee356159183866e409cf81e
Karimus <karim.koriche@yahoo.fr>
2016-01-31 20:43:44 +0100

    Initial Commit

---
+++ GingersnapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GingersnapTest
+//
+//  Created by Karim Koriche on 31/01/2016.
+//  Copyright © 2016 Karim Koriche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GingersnapTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GingersnapTest
+//
+//  Created by Karim Koriche on 31/01/2016.
+//  Copyright © 2016 Karim Koriche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
